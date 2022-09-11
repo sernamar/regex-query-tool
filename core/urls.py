3d649599ba08d154b78d74a1a4018d2b2eb99a14
sernamar <sernamar@protonmail.com>
2022-09-11 08:55:24 +0200

    Update matches when the text to match changes

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("matches/", views.render_matches, name="matches"),
 
     # password change path to redirect to "index" after success
     path("accounts/password/change/", PasswordChangeView.as_view(), name="account_change_password"),
--- core/views.py
+++ core/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_GET
 
 from utils.utils import get_matches
 
+initial_text = "Some random text"
+initial_pattern = "[A-Z]\\w+"
 
-@require_GET
+
 def index(request):
     if request.htmx:
-        pattern = request.GET.get("pattern")
-        string = request.GET.get("test-string")
-        matches = get_matches(pattern, string)
-        return render(request, "matches.html", {"matches": matches})
+        text = request.POST.get("text")
+        request.session["text"] = text
+
+        pattern = request.session.get("pattern")
+        print("Pattern in session:", pattern)
+        matches = get_matches(pattern, text)
+        return render(request, "matches.html", {"text": text, "matches": matches})
     else:
-        return render(request, "index.html")
+        if request.session.get("pattern") is None:
+            request.session["pattern"] = initial_pattern
+        pattern = request.session.get("pattern")
+        if request.session.get("text") is None:
+            request.session["text"] = initial_text
+        text = request.session.get("text")
+        matches = get_matches(pattern, text)
+        return render(request, "index.html", {"pattern": pattern, "text": text, "matches": matches})
+
+
+def render_matches(request):
+    pattern = request.POST.get("pattern")
+    request.session["pattern"] = pattern
+
+    text = request.session.get("text")
+    matches = get_matches(pattern, text)
+    return render(request, "matches.html", {"text": text, "matches": matches})
