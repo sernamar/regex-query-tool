fc60f0e9e97c03efd5731ce1242af8a16282ddec
sernamar <sernamar@protonmail.com>
2022-09-11 10:12:28 +0200

    Add functions to add and remove some style to strings

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,24 @@
 import re
 
+CLEANER = re.compile("<.*?>")  # global, to avoid compiling it every time the "remove_style" function is called
+
 
 def get_matches(pattern, string):
     return re.findall(pattern, string)
+
+
+def style_word(word):
+    """"
+    Adds a "color red" style to the word.
+    """
+    return '<span style="color: red;">' + word + '</span>'
+
+
+def add_style(string, matches):
+    words_list = [style_word(word) if word in matches else word
+                  for word in string.split()]
+    return " ".join(words_list)
+
+
+def remove_style(string):
+    return re.sub(CLEANER, "", string)
